f326f96694f6a89d48f19fa11b94abc24d6bf1ed
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-08-23 13:17:54 +0300

    Initialization. Added splash screen and first menu screen.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start() {
+		Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+
+	public void LoadLevel(string name) {
+		Application.LoadLevel(name);
+	}
+
+	public void QuitRequest() {
+		Application.Quit();
+	}
+
+	public void LoadNextLevel() { //You have to consider the build settings with this, they have to be in the right order.
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
