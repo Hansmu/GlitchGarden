b8e9d8eb659ea8e7ad70f4df806f1c919b7f560f
hansmu <hans.miikael@gmail.com>
2016-12-11 13:31:38 +0200

    Projectiles deal damage.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -15,6 +15,16 @@ public class Projectile : MonoBehaviour {
 		transform.Translate(Vector3.right * speed * Time.deltaTime);
 	}
 
+	void OnTriggerEnter2D(Collider2D collider) {
+		Attacker attacker = collider.gameObject.GetComponent<Attacker>();
+		Health health = collider.gameObject.GetComponent<Health>();
+
+		if (attacker && health) {
+			health.DealDamage(damage);
+			Destroy(gameObject);
+		}
+	}
+
 	void onBecameInvisible() { //When no camera can see the object.
 		
 	}
