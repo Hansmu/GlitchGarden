4271dbf46da3e9a65d9aee21b939295fe99c1059
hansmu <hans.miikael@gmail.com>
2016-12-11 14:01:56 +0200

    Added buttons to place defenders.

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	public GameObject defenderPrefab;
+	public static GameObject selectedDefender;
+
+	private Button[] buttonArray;
+
+	// Use this for initialization
+	void Start () {
+		buttonArray = GameObject.FindObjectsOfType<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		SetAllButtonsToBlack();
+
+		GetComponent<SpriteRenderer>().color = Color.white;
+		selectedDefender = defenderPrefab;
+	}
+
+	void SetAllButtonsToBlack() {
+		foreach(Button currentButton in buttonArray) {
+			currentButton.GetComponent<SpriteRenderer>().color = Color.black;
+		}
+	}
+}
