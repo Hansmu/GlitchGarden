7a8eba81da2f6302d3fdf461942df6922a93fd76
hansmu <hans.miikael@gmail.com>
2016-12-21 12:37:12 +0200

    Minor tweaks.

---
+++ Assets/Scripts/ScoreCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreCollider : MonoBehaviour {
+
+	private LevelManager levelManager;
+
+	void Start () {
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+
+	void OnTriggerEnter2D() {
+		levelManager.LoadLevel("04_Score");	
+	}
+}
